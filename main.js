930db829faa2aae1abca94f74c935c2a88359dc8
Thusara Senanayake <89896736+thusarasenanayake@users.noreply.github.com>
2023-05-28 23:48:28 +0530

    feat: add work experience section

---
+++ main.js
@@ -0,0 +1,2 @@
+import "./input.css";
+import "/node_modules/ionicons/dist/ionicons/ionicons.esm.js"
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './main.js'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['quicksand'],
+        'poppins' : 'poppins'
+      },
+    
+    },
+  },
+  plugins: [],
+}
+
