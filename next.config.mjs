f5cd71025862780e9c3fafdd780fd24f378b7826
Thusara Senanayake <t89896736+thusarasenanayake@users.noreply.github.com>
2024-10-17 20:32:21 +0530

    chore: change next config and env

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,39 @@
-const isGithubActions =
-  (process.env.GITHUB_ACTIONS && process.env.GITHUB_REPOSITORY_URL) || false;
-let assetPrefix = "";
-let basePath = "";
+// const isGithubActions =
+//   (process.env.GITHUB_ACTIONS && process.env.GITHUB_REPOSITORY_URL) || false;
+// let assetPrefix = "";
+// let basePath = "";
 
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY_URL.replace(/.*?\//, "");
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY_URL.replace(/.*?\//, "");
 
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
+//   assetPrefix = `/${repo}/`;
+//   basePath = `/${repo}`;
+// }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+//   output: "export",
 
-  /**
-   * Set base path. This is usually the slug of your repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath,
-  assetPrefix,
+//   /**
+//    * Set base path. This is usually the slug of your repository.
+//    *
+//    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+//    */
+//   basePath,
+//   assetPrefix,
 
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-    // loader: '',
-    // path: '',
-  },
+//   /**
+//    * Disable server-based image optimization. Next.js does not support
+//    * dynamic features with static exports.
+//    *
+//    * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+//    */
+//   images: {
+//     unoptimized: true,
+//     // loader: '',
+//     // path: '',
+//   },
 };
 
 export default nextConfig;
